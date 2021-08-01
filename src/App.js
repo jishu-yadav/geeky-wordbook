b5e262d654dd03351122a3759ebcb10819c530c0
jishu-yadav <jishuyadavcute88@gmail.com>
2021-08-01 12:59:15 +0530

    wordbook

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { React, useState } from "react";
+import Axios from "axios";
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
+import { FcSpeaker } from "react-icons/fc";
+
+function App() {
+// Setting up the inital states using react hook 'useState'
+
+const [data, setData] = useState("");
+const [searchWord, setSearchWord] = useState("");
+
+// Function to fetch information on button
+// click, and set the data accordingly
+function getMeaning() {
+	Axios.get(
+	`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
+	).then((response) => {
+	setData(response.data[0]);
+	});
+}
+
+// Function to play and listen the
+// phonetics of the searched word
+function playAudio() {
+	let audio = new Audio(data.phonetics[0].audio);
+	audio.play();
+}
+
+return (
+	<div className="App">
+	<h1>Free Dictionary World</h1>
+	<div className="searchBox">
+
+	
+		<input
+		type="text"
+		placeholder="Search..."
+		onChange={(e) => {
+			setSearchWord(e.target.value);
+		}}
+		/>
+		<button
+		onClick={() => {
+			getMeaning();
+		}}
+		>
+		<FaSearch size="20px" />
+		</button>
+	</div>
+	{data && (
+		<div className="showResults">
+		<h2>
+      {data.word}{" "}
+			<button
+			onClick={() => {
+				playAudio();
+			}}
+			>
+			<FcSpeaker size="26px" />
+			</button>
+		</h2>
+		<h4>Part of speech:</h4>
+
+		<p>{data.meanings[0].partOfSpeech}</p>
+
+		<h4>Word Definition:</h4>
+
+		<p>{data.meanings[0].definitions[0].definition}</p>
+
+		<h4>Sample Sentences:</h4>
+
+		<p>{data.meanings[0].definitions[0].example}</p>
+		</div>
+	)}
+	</div>
+);
+}
+
+export default App;
